52e2ee2d3f889a0c3a10c2e6081b4c9d96526c1b
퐁치퐁치 <zxcv0620@gmail.com>
2023-09-25 13:56:13 +0000

    not solving 9017. reason : runtime error

--- view8708/9017/main.cpp
+++ view8708/9017/main.cpp
@@ -1,22 +1,16 @@
 // URL : https://www.acmicpc.net/problem/9017
 
-#include <algorithm>
 #include <iostream>
-#include <vector>
-#include <utility>
+#include<vector>
+#include <map>
 
 using namespace std;
-
-typedef pair<int, int> pii;
 
 const int MAX_PLAYER_SIZE = 1'005;
 int record[MAX_PLAYER_SIZE];
 
 const int MAX_TEAM_SIZE = 205;
-int scores[MAX_TEAM_SIZE];
-vector<int> teams[MAX_TEAM_SIZE];
-int teams_count[MAX_TEAM_SIZE];
-int cutline[MAX_TEAM_SIZE];
+int player_count[MAX_TEAM_SIZE];
 
 int main(void) {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
@@ -24,39 +18,33 @@ int main(void) {
 	int T; cin >> T;
 	for (int t=0; t < T; t++) {
 		int N; cin >> N;
-		int max_team = 0;
+
+		map<int, vector<int>> team;
+		map<int, int> scores;
 
 		fill_n(record, N, 0);
-		fill_n(teams, MAX_TEAM_SIZE, vector<int>());
-		fill_n(teams_count, MAX_TEAM_SIZE, 0);
-		fill_n(scores, MAX_TEAM_SIZE, 0);
-		fill_n(cutline, MAX_TEAM_SIZE, 0);
+		fill_n(player_count, MAX_TEAM_SIZE, 0);
 
 		for (int i=0; i < N; i++) {
 			cin >> record[i];
-			teams_count[record[i]]++;
-
-			if (max_team < record[i]) max_team = record[i];
+			player_count[record[i]]++;
 		}
 		
 		int rank = 1;
 		for (int i=0; i < N; i++) {
-			if (teams_count[ record[i] ] == 6) {
-				teams[ record[i] ].push_back(rank);
-
-				if (cutline[record[i]] < 4) {
-					scores[record[i]] += rank;
-					cutline[record[i]]++;
+			if (player_count[ record[i] ] == 6) {
+				team[ record[i] ].push_back(rank++);
+				if (team[record[i]].size() <= 4) {
+					if (scores.find(record[i]) == scores.end()) scores[record[i]] = 0;
+					scores[record[i]] += rank-1;
 				}
-				rank++;
 			}
 		}
-		
-		int result = 1;
-		for (int i=1; i <= max_team; i++) {
-			if (scores[i] == 0) continue;
-			if ((scores[result] > scores[i]) || (scores[result] == scores[i] && teams[result][4] > teams[i][4]))
-				result = i;
+
+		int result = record[0];
+		for (auto score : scores) {
+			if ((scores[result] > score.second) || (scores[result] == score.second && team[result][4] > team[score.first][4]))
+				result = score.first;
 		}
 
 		cout << result << '\n';
