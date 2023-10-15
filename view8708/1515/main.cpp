3ce2f20caf9783d29ccaea76c00522e0442737b2
퐁치퐁치 <zxcv0620@gmail.com>
2023-10-15 07:16:03 +0000

    not solving 1515

--- view8708/1515/main.cpp
+++ view8708/1515/main.cpp
@@ -5,38 +5,57 @@
 
 using namespace std;
 
-int count_number(int n, int target);
+int includeCount(int, int);
+int number_length(int);
+
 int main(void) {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
 
     string num;
     cin >> num;
 
-    int lastest = 0;
+    int lastest = 1;
     for (int i = 0; i < num.length(); i++) {
-        lastest++;
-        while (true) {
-            int count = count_number(lastest, num[i]-'0');
-            if (count != 0) {
-                lastest += count - 1;
-                break;
+        bool flag = false;
+        while (!flag) {
+            int length = (i+number_length(lastest) < num.length() ? number_length(lastest) : 1);
+            int current_number = stoi(num.substr(i, length));
+            if (current_number == lastest) {
+                flag = true;
+                i += length-1;
             }
-            else lastest++;
+            else {
+                int count = includeCount(lastest, num[i]-'0');
+                if (count != 0) {
+                    i += count-1;
+                    flag = true;
+                }
+            }
+            lastest++;
         }
     }
 
-    cout << lastest << '\n';
+    cout << lastest-1 << '\n';
     
     return 0;
 }
 
-int count_number(int n, int target) {
+int includeCount(int n, int target) {
     if (n == target) return 1;
 
     int result = 0;
     do {
         if (n%10 == target) result++;
         n /= 10;
-    } while (n != 0) ;
+    } while (n != 0);
+    return result;
+}
+
+int number_length(int n) {
+    int result = 0;
+    while (n) {
+        n /= 10;
+        result++;
+    }
     return result;
 }
