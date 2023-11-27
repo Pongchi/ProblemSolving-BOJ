aecfb194e0637ce22fbb3e312fad94bc427164a6
퐁치퐁치 <zxcv0620@gmail.com>
2023-11-27 14:02:52 +0000

    wtf !

--- programmers/49191/main.py
+++ programmers/49191/main.py
@@ -1,23 +1,23 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/49191
 
 def solution(n, results):
-    WIN, MYSTERY, LOSE = [1, 0, -1]
-    graph = [ [MYSTERY] * (n+1) for _ in range(n+1) ]
+    win = {i: set() for i in range(1, n+1)}
+    lose = {i: set() for i in range(1, n+1)}
     for A, B in results:
-        graph[A][B] = WIN
-        graph[B][A] = LOSE
+        win[A].add(B)
+        lose[B].add(A)
 
-    for k in range(1, n+1):
-        for i in range(1, n+1):
-            for j in range(1, n+1):
-                if WIN == graph[i][k] == graph[k][j]:
-                    graph[i][j] = WIN
-                    graph[j][i] = LOSE
-    
+    for i in range(1, n+1):
+        for loser in win[i]: # i에게 진 loser를 이긴 사람들의 목록에 i를 이긴 사람들의 목록을 추가한다.
+            lose[loser].update(lose[i])
+        for winner in lose[i]: # i에게 이긴 winner에게 진 사람들의 목록에 i에게 진 사람들의 목록을 추가한다.
+            win[winner].update(win[i])
+
     result = 0
     for i in range(1, n+1):
-        if graph[i].count(MYSTERY) == 2:
+        if len(win[i]) + len(lose[i]) == n-1:
             result += 1
+
     return result
 
 print(solution(
