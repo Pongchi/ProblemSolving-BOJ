82214c514d017903ab2ca6738b3cbad8dcf2c23e
퐁치퐁치 <zxcv0620@gmail.com>
2023-12-05 13:57:00 +0000

    new way! programmers 17685 with Trie

--- programmers/17685/main.py
+++ programmers/17685/main.py
@@ -1,3 +1,4 @@
+'''
 def solution(words):
     result = [0] * len(words)
     words.sort()
@@ -15,6 +16,30 @@ def solution(words):
         result[i+1] = max(result[i+1], min(len(words[i+1]), overlap+1))
 
     return sum(result)
+'''
+
+def solution(words):
+    Trie = dict()
+    for word in words:
+        current_trie = Trie
+        for char in word:
+            if char not in current_trie:
+                current_trie[char] = {'count': 0}
+            current_trie[char]['count'] += 1
+            current_trie = current_trie[char]
+
+    result = 0
+    for word in words:
+        current_trie = Trie
+        for i, char in enumerate(word):
+            current_trie = current_trie[char]
+            if current_trie['count'] == 1:
+                result += i+1
+                break
+        else:
+            result += len(word)
+
+    return result
 
 print(solution(
     ["go", "gone", "guild"]
