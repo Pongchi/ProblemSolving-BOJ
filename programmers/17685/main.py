bd0649cbfbc54f0eaa31886f1944bdde17358ce2
퐁치퐁치 <zxcv0620@gmail.com>
2023-12-05 13:23:57 +0000

    solving programmers 17685

---
+++ programmers/17685/main.py
@@ -0,0 +1,29 @@
+def solution(words):
+    result = [0] * len(words)
+    words.sort()
+    
+    for i in range(len(words)-1):
+        overlap = 0
+        for length in range(min(len(words[i]), len(words[i+1]))):
+            if words[i][length] != words[i+1][length]:
+                overlap = length
+                break
+        else:
+            overlap = min(len(words[i]), len(words[i+1]))
+
+        result[i] = max(result[i], min(len(words[i]), overlap+1))
+        result[i+1] = max(result[i+1], min(len(words[i+1]), overlap+1))
+
+    return sum(result)
+
+print(solution(
+    ["go", "gone", "guild"]
+), 7)
+
+print(solution(
+    ["abc", "def", "ghi", "jklm"]
+), 4)
+
+print(solution(
+    ["word", "war", "warrior", "world"]
+), 15)
