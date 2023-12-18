214312c3421a49ccf2ec89094514fd0d2d6d55cd
퐁치퐁치 <zxcv0620@gmail.com>
2023-12-18 13:57:45 +0000

    solving 1074

--- solved.ac/class3/1074/main.py
+++ solved.ac/class3/1074/main.py
@@ -10,17 +10,16 @@ def divide_and_conquer(N, R, C, offset):
     else:
         unit = 2 ** (N-1) 
         diff = ((2 ** N) * (2 ** N)) // 4
-        result = divide_and_conquer(N-1, R, C, offset) # 1사분면
-        if result != -1: return result
+        if R <= r <= R+unit-1 and C <= c <= C+unit-1:
+            return divide_and_conquer(N-1, R, C, offset) # 1사분면
+        elif R <= r <= R+unit-1 and C+unit <= c <= C+unit*2-1:
+            return divide_and_conquer(N-1, R, C + unit, offset + (diff * 1)) # 2사분면
 
-        result = divide_and_conquer(N-1, R, C + unit, offset + (diff * 1)) # 2사분면
-        if result != -1: return result
+        elif R + unit <= r <= R+unit*2-1 and C <= c <= C+unit-1:
+            return divide_and_conquer(N-1, R + unit, C, offset + (diff * 2)) # 3사분면
+        else:
+            return divide_and_conquer(N-1, R + unit, C + unit, offset + (diff * 3)) # 4사분면
 
-        result = divide_and_conquer(N-1, R + unit, C, offset + (diff * 2)) # 3사분면
-        if result != -1: return result
-
-        result = divide_and_conquer(N-1, R + unit, C + unit, offset + (diff * 3)) # 4사분면
-        if result != -1: return result
         return -1
         
 
