98a3dd1e9a4689fa5b87dae355d28c3ad76ec37b
퐁치퐁치 <zxcv0620@gmail.com>
2024-01-20 03:07:40 +0000

    not solving

--- anything/14003/main.py
+++ anything/14003/main.py
@@ -8,10 +8,27 @@ input = sys.stdin.readline
 A = int(input())
 seq = list(map(int, input().split()))
 lis = [seq[0]]
+dp = [0] * A
 
 for i in range(1, A):
     if lis[-1] < seq[i]:
         lis.append(seq[i])
+        dp[i] = len(lis)
     else:
-        idx = bisect_left(lis, seq[i])
-        lis[idx] = seq[i]
+        dp[i] = bisect_left(lis, seq[i])
+        lis[dp[i]] = seq[i]
+
+max_len = max(dp)
+print(max_len)
+
+real_lis = []
+for i in range(A-1, -1, -1):
+    if dp[i] == max_len:
+        real_lis.append(seq[i])
+        max_len -= 1
+
+print(*real_lis[::-1])
+
+
+print(lis)
+print(dp)
