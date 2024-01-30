df88dd314ed0905d89994c0beafb4fcfcae1cc9d
퐁치퐁치 <zxcv0620@gmail.com>
2024-01-30 10:32:13 +0000

    solving 5639

--- solved.ac/class4/5639/main.py
+++ solved.ac/class4/5639/main.py
@@ -4,40 +4,20 @@ import sys
 
 sys.setrecursionlimit(10**5)
 
-class Node:
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
+keys = list(map(int, sys.stdin.readlines()))
+def postorder(start, end):
+    if start >= end: return
 
-class BinarySearchTree:
-    def __init__(self):
-        self.root = None
+    point = end
+    for i in range(start+1, end):
+        if keys[start] < keys[i]:
+            point = i
+            break
 
-    def insert(self, key):
-        self.root = self._insert(self.root, key)
-        return self.root is not None
-    
-    def _insert(self, node, key):
-        if node is None:
-            return Node(key)
-        
-        if key < node.key:
-            node.left = self._insert(node.left, key)
-        else:
-            node.right = self._insert(node.right, key)
+    postorder(start+1, point)
+    postorder(point, end)
+    print(keys[start])
 
-        return node
+    return
 
-tree = BinarySearchTree()
-keys = sys.stdin.readlines()
-for key in keys:
-    tree.insert(int(key))
-
-def postorder(node):
-    if node:
-        postorder(node.left)
-        postorder(node.right)
-        print(node.key)
-    
-postorder(tree.root)
+postorder(0, len(keys))
