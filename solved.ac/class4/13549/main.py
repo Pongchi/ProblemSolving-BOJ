ed98eb10b897b3cf87e7e27bb1780742f9a0fb0b
퐁치퐁치 <zxcv0620@gmail.com>
2024-01-31 09:40:05 +0000

    solving 13549

--- solved.ac/class4/13549/main.py
+++ solved.ac/class4/13549/main.py
@@ -6,21 +6,22 @@ N, K = map(int, input().split())
 dp = [-1] * 100005
 
 def bfs():
-    queue = deque([[0, N]])
+    queue = deque([N])
+    dp[N] = 0
     while queue and dp[K] == -1:
-        time, current_location = queue.popleft()
+        current_location = queue.popleft()
         
         if 0 <= current_location*2 <= 100000 and dp[current_location*2] == -1:
-            queue.append([time, current_location*2])
-            dp[current_location*2] = time
+            queue.append(current_location*2)
+            dp[current_location*2] = dp[current_location]
         
         if 0 <= current_location-1 <= 100000 and dp[current_location-1] == -1:
-            queue.append([time+1, current_location-1])
-            dp[current_location-1] = time+1
+            queue.append(current_location-1)
+            dp[current_location-1] = dp[current_location] + 1
 
         if 0 <= current_location+1 <= 100000 and dp[current_location+1] == -1:
-            queue.append([time+1, current_location+1])
-            dp[current_location+1] = time+1
+            queue.append(current_location+1)
+            dp[current_location+1] = dp[current_location] + 1
 
 bfs()
 print(dp[K])
