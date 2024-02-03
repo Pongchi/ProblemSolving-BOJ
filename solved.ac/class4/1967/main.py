26d6df2b183cc12c46cd9573914a66bcc9877c29
퐁치퐁치 <zxcv0620@gmail.com>
2024-02-03 06:28:50 +0000

    solving 1967

--- solved.ac/class4/1967/main.py
+++ solved.ac/class4/1967/main.py
@@ -11,20 +11,20 @@ graph = [ [] for _ in range(n+1) ]
 for line in lines[1:]:
     a, b, c = map(int, line.split())
     graph[a].append((b, c))
+    graph[b].append((a, c))
 
-diameters = [0] * (n+1)
-def dfs(parent):
-    result = []
-    for child, weight in graph[parent]:
-        result.append(dfs(child)+weight)
+def calc_distances_from(start):
+    distances = [-1] * (n+1)
+    distances[start] = 0
+    def dfs(parent, distance):
+        for child, weight in graph[parent]:
+            if distances[child] == -1:
+                distances[child] = distance + weight
+                dfs(child, distances[child])
 
-    diameters[parent] = max(result + [0])
-    return diameters[parent]
+    dfs(start, 0)
+    return distances
 
-dfs(1)
-result = 0
-for parent in range(1, n+1):
-    tmp = sorted([diameters[child] + weight for child, weight in graph[parent]])
-    result = max(result, sum(tmp[:2]))
-
-print(result)
+distances = calc_distances_from(1)
+start = distances.index(max(distances))
+print(max(calc_distances_from(start)))
