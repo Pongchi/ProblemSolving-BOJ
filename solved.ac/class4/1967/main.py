b31346b636c677cc9767e4046e7c736f9b60ffea
퐁치퐁치 <zxcv0620@gmail.com>
2024-02-03 06:02:33 +0000

    not solving 1967

---
+++ solved.ac/class4/1967/main.py
@@ -0,0 +1,33 @@
+# https://www.acmicpc.net/problem/1967
+
+import sys
+
+sys.setrecursionlimit(10**5)
+
+lines = sys.stdin.readlines()
+
+n = int(lines[0])
+graph = [ [] for _ in range(n+1) ]
+for line in lines[1:]:
+    a, b, c = map(int, line.split())
+    graph[a].append((b, c))
+
+diameters = [0] * (n+1)
+def dfs(parent):
+    result = []
+    for child, weight in graph[parent]:
+        result.append(dfs(child)+weight)
+
+    diameters[parent] = max(result + [0])
+    return diameters[parent]
+
+dfs(1)
+result = 0
+for parent in range(1, n+1):
+    tmp = 0
+    for child, weight in graph[parent]:
+        tmp += diameters[child] + weight
+
+    result = max(result, tmp)
+
+print(result)
